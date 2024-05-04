9700063f083c9d675aa9a0cfabbd15bd8082dcea
Elleined <demadegu@gmail.com>
2024-05-05 00:20:00 +0800

    Added code sample for access object, array, and variable from ts to html file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angular-notes';
+  myVariable: any = "My Variable";
+
+  myVariables: Array<number> = [1, 2, 3];
+
+  myObject: Person = {
+    name: "Person Name",
+    age: 100
+  }
+}
+
+interface Person {
+  name: string;
+  age: number;
 }
