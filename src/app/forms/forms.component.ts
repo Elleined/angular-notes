293984e07638d376b3438a93f0d51bb127cb32a1
Elleined <demadegu@gmail.com>
2024-05-14 23:02:38 +0800

    Added form builder sample for nested form groups

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
 
+  registerForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.registerForm = formBuilder.group({
+      informationGroup: formBuilder.group({
+        nameControl: ['', Validators.required],
+        ageControl: ['', Validators.required]
+      }),
+      addressGroup: formBuilder.group({
+        provinceControl: ['', Validators.required],
+        cityControl: ['', Validators.required],
+        baranggayControl: ['', Validators.required]
+      })
+    });
+  }
+  
+  nestedFormOnSubmit(): void {
+    console.log(this.registerForm.value);
+  }
+  
 }
